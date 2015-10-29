8bcd268a84d6b3c67372ef702ca52e40ac5b687e
LimeStreem <LimeStreem@gmail.com>
2015-10-29 13:15:19 -0700

    completed assignment

---
+++ Assets/InputController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+	public Sprite firstTexture;
+
+	public Sprite secoundTexture;
+
+	private bool textureFlag = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		this.GetComponent<SpriteRenderer> ().sprite = this.textureFlag ? this.firstTexture : this.secoundTexture;
+		this.textureFlag = !this.textureFlag;
+	}
+}
---
+++ Assets/KeyboardController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position += new Vector3 (1, 0, 0) * Input.GetAxis ("Horizontal");
+		this.transform.position += new Vector3 (0, 1, 0) * Input.GetAxis ("Vertical");
+	}
+}
